69a38c02f6a2ae1072606be6293d5c83bafe4ae5
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-05-21 12:02:59 +0200

    Vypnutí

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import pygame
 import sys
 
 pygame.init()
+clock = pygame.time.Clock()
+
+WIDTH, HEIGHT = 800, 600
 
 window = pygame.display.set_mode((800, 600))
+
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
