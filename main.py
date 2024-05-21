4fd3d01c8de4cc713512243c5d7381d22d71008d
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-05-21 12:09:40 +0200

    update

--- main.py
+++ main.py
@@ -8,11 +8,21 @@ WIDTH, HEIGHT = 800, 600
 
 window = pygame.display.set_mode((800, 600))
 
-
+class Snake:
+    def __init__(self, length, position, direction) -> None:
+        self.length = length
+        self.position = position
+        self.direction = direction
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_ESCAPE]:
+        pygame.quit()
+        sys.exit()
 
