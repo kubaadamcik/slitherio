4b56c7a958291ea95b6a9d9d0305d9644d211c5a
Jakub Adamčík <59259adamcik@student.sstebrno.cz>
2024-05-23 21:09:57 +0200

    Ocas pořád nefunguje

--- main.py
+++ main.py
@@ -19,17 +19,19 @@ font = pygame.font.Font(None, 36)
 # temp variables
 
 class Snake:
-    def __init__(self, length, position, player_speed, tail_size, player_size) -> None:
+    def __init__(self, length, player_position, player_speed, tail_size, player_size) -> None:
         self.length = length
         self.tail_size = tail_size
-        self.position = position
+        self.player_position = player_position
         self.player_size = player_size
         self.player_speed = player_speed
-        self.head = pygame.rect.Rect((*self.position, *self.player_size))
+        self.head = pygame.rect.Rect((*self.player_position, *self.player_size))
         self.tails = []
         self.right = False
         self.up = False
-        self.direction = [1, 0]
+        self.new_direction = [1, 0]
+        self.old_direction = [1, 0]
+        self.turning_pos = self.player_position
 
         
     
@@ -46,32 +48,50 @@ class Snake:
     def move(self, keys) -> None:
 
         if keys[pygame.K_UP]:
-            self.direction[1] = -1
-            self.direction[0] = 0
+            self.new_direction[1] = -1
+            self.new_direction[0] = 0
+            self.turning_pos = self.player_position
         if keys[pygame.K_DOWN]:
-            self.direction[1] = 1
-            self.direction[0] = 0
+            self.new_direction[1] = 1
+            self.new_direction[0] = 0
+            self.turning_pos = self.player_position
         if keys[pygame.K_LEFT]:
-            self.direction[0] = -1
-            self.direction[1] = 0
+            self.new_direction[0] = -1
+            self.new_direction[1] = 0
+            self.turning_pos = self.player_position
         if keys[pygame.K_RIGHT]:
-            self.direction[0] = 1
-            self.direction[1] = 0
+            self.new_direction[0] = 1
+            self.new_direction[1] = 0
+            self.turning_pos = self.player_position
+        
 
-        self.position = (self.position[0] + self.direction[0] * self.player_speed, self.position[1] + self.direction[1] * self.player_speed)
-        self.position = (max(0, min(self.position[0], WIDTH - self.player_size[0])), max(0, min(self.position[1], HEIGHT - self.player_size[1])))
+        self.player_position = (self.player_position[0] + self.new_direction[0] * self.player_speed, self.player_position[1] + self.new_direction[1] * self.player_speed)
+        self.player_position = (max(0, min(self.player_position[0], WIDTH - self.player_size[0])), max(0, min(self.player_position[1], HEIGHT - self.player_size[1])))
 
-        self.head = pygame.rect.Rect((*self.position, *self.player_size))
+        self.head = pygame.rect.Rect((*self.player_position, *self.player_size))
 
-        # TODO: Otáčéní ocasu
+        # TODO: Ocas pronásleduje hada postupně
         for i in range(self.length):
-            x = self.position[0] - self.tail_size[0] * self.direction[0] * (i + 1)
-            y =  self.position[1] - self.tail_size[1] * self.direction[1] * (i + 1)
-            self.tails.append(pygame.rect.Rect(x, y, *self.tail_size))
+            x = self.player_position[0] - self.tail_size[0] * self.old_direction[0] * (i + 1)
+            y =  self.player_position[1] - self.tail_size[1] * self.old_direction[1] * (i + 1)
+
+            if x == self.turning_pos[0] and y == self.turning_pos[1]:
+                self.old_direction = self.new_direction
+                x = self.player_position[0] - self.tail_size[0] * self.new_direction[0] * (i + 1)
+                y =  self.player_position[1] - self.tail_size[1] * self.new_direction[1] * (i + 1)
+                self.tails.append(pygame.rect.Rect(x, y, *self.tail_size))
+            else:
+                self.new_direction = self.old_direction
+                self.tails.append(pygame.rect.Rect(x, y, *self.tail_size))
 
 
-    def eat(self, fruit):
-        return self.head.colliderect(fruit.fruit)        
+
+    def check_eat(self, fruit):
+        return self.head.colliderect(fruit.fruit)
+    
+    # TODO: Dodělat
+    def check_death(self, tail, border):
+        pass
 
     
 
@@ -99,7 +119,7 @@ def ShowText(text):
 fruit = Fruit((randrange(10, 990), randrange(10, 790)), FRUIT_SIZE)
 
 
-player1 = Snake(1, (10, 10), 2, TAIL_SIZE, PLAYER_SIZE)
+player1 = Snake(1, (10, 10), 1, TAIL_SIZE, PLAYER_SIZE)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -122,7 +142,7 @@ while True:
 
     player1.move(keys)
 
-    if player1.eat(fruit):
+    if player1.check_eat(fruit):
         player1.length += 1
 
         fruit.spawn((randrange(10, 990, 10), randrange(10, 790, 10)))
